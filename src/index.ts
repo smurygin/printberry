bbb2bd81d2f918f15203c2f4a88e71a27b8dd3ca
Dmitry Smurygin <d.a.smurygin@gmail.com>
2022-09-08 13:45:24 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,23 @@
 const escpos = require('escpos');
-// install escpos-usb adapter module manually
-escpos.USB = require('escpos-usb');
-// Select the adapter based on your printer type
-const device = new escpos.USB();
-// const device  = new escpos.Network('localhost');
-// const device  = new escpos.Serial('/dev/usb/lp0');
+escpos.SerialPort = require('escpos-serialport');
+
+const serialDeviceOnLinux = new escpos.SerialPort('/dev/usb/lp0', {
+  baudRate: 14400,
+  stopBit: 2,
+});
 
 const options = { encoding: 'GB18030' /* default */ };
-// encoding is optional
 
-const printer = new escpos.Printer(device, options);
+const printer = new escpos.Printer(serialDeviceOnLinux, options);
 
-device.open(function (error) {
+serialDeviceOnLinux.open(() => {
   printer
-    .font('a')
-    .align('ct')
-    .style('bu')
+    .font('A')
+    .align('CT')
+    .style('BU')
     .size(1, 1)
     .text('The quick brown fox jumps over the lazy dog')
-    .text('敏捷的棕色狐狸跳过懒狗')
+    .text('Русский текст')
     .barcode('1234567', 'EAN8')
-    .table(['One', 'Two', 'Three'])
-    .tableCustom(
-      [
-        { text: 'Left', align: 'LEFT', width: 0.33, style: 'B' },
-        { text: 'Center', align: 'CENTER', width: 0.33 },
-        { text: 'Right', align: 'RIGHT', width: 0.33 },
-      ],
-      { encoding: 'cp857', size: [1, 1] }, // Optional
-    )
-    .qrimage('https://github.com/song940/node-escpos', function (err) {
-      this.cut();
-      this.close();
-    });
+    .close();
 });
